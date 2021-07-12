9a06fd9c607b388cc8cfd8e7052013fc39017039
FernB <fernbradder@gmail.com>
2021-07-12 10:04:00 +0800

    fix up database

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect
+import json
 
 from flask import Flask, jsonify
 
@@ -51,14 +52,24 @@ def default():
 
 
 
-@app.route("/api/d")
+@app.route("/api/Othernotifiablediseases")
 def diseasegroups():
 
-    Disease = Base.classes.Bloodbornediseases
+    
+    Disease = Base.classes.DiseaseSummary
 
     session = Session(engine)
 
-    # results = session.query(Disease).filter(Disease.Location == "WA").all()
+    results = session.query(Disease).filter(Disease.Location == "WA").filter(Disease.Disease_Group == "Bloodborne diseases")
+    listofdiseases = session.query(Disease.Disease_Name).filter(Disease.Location == "WA").filter(Disease.Disease_Group == "Bloodborne diseases").group_by(Disease.Disease_Name).all()
+    all_names = list(np.ravel(listofdiseases))
+
+
+
+       
+    # inspector = inspect(engine)
+    # all_names = inspector.get_table_names()
+
 
     # inspector = inspect(engine)
     # all_names = Base.classes.keys()
@@ -76,13 +87,56 @@ def diseasegroups():
     # datalist.append([result[1] for result in results])
 
     # all_names = list(np.ravel(results))
-    results = session.query(Disease.Location).group_by(Disease.Location).all()
+    # results = session.query(Disease.Location).group_by(Disease.Location).all()
 
     # session.close()
 
-    all_names = list(np.ravel(results))
+    # all_names = list(np.ravel(results))
 
-    return jsonify(all_names)
+#   "Legionellosis", 
+#   "Leprosy", 
+#   "Meningococcal disease (invasive)", 
+#   "RSV", 
+#   "Tuberculosis", 
+#   "iGAS", 
+#   "Location", 
+#   "Year"
+
+
+    
+    disease_list = []
+
+
+    disease_dic = {}
+    for name in all_names:
+        disease_dic[name] = [result.Infection_Rate for result in results if result.Disease_Name == name]
+    disease_list.append(disease_dic)
+  
+
+
+    # disease_dic["Meningococcal disease (invasive)"] = [result.like("%Meningococcal%") for result in results]
+    # for row in results:
+    #     print(row.__dict__)
+        # dd = {k: v for k,v in d.items() if not k.startswith('_')}
+
+
+        # disease_list.append(d)
+
+    # dies = []
+    # for i in td:
+    #     for j in results:
+    #         ddi = {}
+    #         ddi[i] = j
+    #         dies.append(ddi)
+
+
+ 
+
+
+    
+
+
+    return jsonify(disease_list)
 
 
 if __name__ == '__main__':
